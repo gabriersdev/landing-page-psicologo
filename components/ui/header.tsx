af83487c95f826c92a8f7b5edb8bfa75f2ab0b1e
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-02-25 15:17:07 -0300

    feat: header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="fixed top-2 z-30 w-full md:top-6">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div
-          className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-xs before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(var(--color-gray-100),var(--color-gray-200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] [&_*]:text-[16px] ">
+          className="relative flex h-14 items-center justify-between gap-3  bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-xs before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(var(--color-gray-100),var(--color-gray-200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] [&_*]:text-[16px]" style={{borderRadius: "8.5px"}}>
           {/* Site branding */}
           <div className="flex flex-1 items-center">
             <Logo/>
@@ -17,7 +17,7 @@ export default function Header() {
             <li>
               <Link
                 href="/signin"
-                className="btn-sm text-gray-800 shadow-sm hover:bg-gray-50 hidden md:block"
+                className="btn-sm text-gray-800 shadow-sm hover:bg-emerald-200 bg-emerald-100 hidden md:block"
               >
                 Início
               </Link>
@@ -25,7 +25,7 @@ export default function Header() {
             <li>
               <Link
                 href="/signin"
-                className="btn-sm text-gray-800 shadow-sm hover:bg-gray-50"
+                className="btn-sm text-gray-800 shadow-sm hover:bg-emerald-200 bg-emerald-100"
               >
                 Sobre
               </Link>
@@ -33,7 +33,7 @@ export default function Header() {
             <li>
               <Link
                 href="/signin"
-                className="btn-sm text-gray-800 shadow-sm hover:bg-gray-50  hidden md:block"
+                className="btn-sm text-gray-800 shadow-sm hover:bg-emerald-200 bg-emerald-100  hidden md:block"
               >
                 Serviços
               </Link>
@@ -41,7 +41,7 @@ export default function Header() {
             <li>
               <Link
                 href="/signin"
-                className="btn-sm text-emerald-700 shadow-sm hover:bg-gray-50"
+                className="btn-sm text-emerald-700 shadow-sm hover:bg-emerald-300 bg-emerald-200"
               >
                 Contato
               </Link>
