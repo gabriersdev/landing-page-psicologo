6351a0ed342901e87253fdc779be487b4644eaa1
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-02-25 15:06:24 -0300

    feat: componente de servicos

---
+++ components/services.tsx
@@ -0,0 +1,62 @@
+const data = [
+  {
+    icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-pulse" viewBox="0 0 16 16"><path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053.918 3.995.78 5.323 1.508 7H.43c-2.128-5.697 4.165-8.83 7.394-5.857q.09.083.176.171a3 3 0 0 1 .176-.17c3.23-2.974 9.522.159 7.394 5.856h-1.078c.728-1.677.59-3.005.108-3.947C13.486.878 10.4.28 8.717 2.01zM2.212 10h1.315C4.593 11.183 6.05 12.458 8 13.795c1.949-1.337 3.407-2.612 4.473-3.795h1.315c-1.265 1.566-3.14 3.25-5.788 5-2.648-1.75-4.523-3.434-5.788-5"/><path d="M10.464 3.314a.5.5 0 0 0-.945.049L7.921 8.956 6.464 5.314a.5.5 0 0 0-.88-.091L3.732 8H.5a.5.5 0 0 0 0 1H4a.5.5 0 0 0 .416-.223l1.473-2.209 1.647 4.118a.5.5 0 0 0 .945-.049l1.598-5.593 1.457 3.642A.5.5 0 0 0 12 9h3.5a.5.5 0 0 0 0-1h-3.162z"/></svg>),
+    title: "Psicoterapia Individual",
+    description: "Atendimento personalizado para adultos, adolescentes e idosos.",
+  },
+  {
+    icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-capsule"
+                viewBox="0 0 16 16">
+      <path
+        d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429z"/>
+    </svg>),
+    title: "Terapia de Ansiedade e Depressão",
+    description: "Abordagem especializada para transtornos emocionais."
+  },
+  {
+    icon: (<svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" className="bi bi-prescription2" viewBox="0 0 16 16"><path d="M7 6h2v2h2v2H9v2H7v-2H5V8h2z"/><path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1zm2 3v10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V4zM3 3h10V1H3z"/></svg>),
+    title: "Autoconhecimento e Desenvolvimento Pessoal",
+    description: "Para aqueles que buscam evoluir e encontrar propósito."
+  },
+  {
+    icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1"/></svg>),
+    title: "Apoio Psicológico Online",
+    description: "Atendimentos virtuais para acessibilidade e comodidade."
+  }
+];
+
+export default function Services() {
+  return (
+    <section className="relative before:absolute before:inset-0 before:-z-20 before:bg-emerald-800">
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="mx-auto max-w-3xl pb-16 text-center md:pb-20">
+            <h2 className="text-3xl font-bold text-gray-200 md:text-4xl">
+              Serviços oferecidos
+            </h2>
+          </div>
+          {/* Grid */}
+          <div
+            className="grid overflow-hidden sm:grid-cols-2 lg:grid-cols-3 *:relative *:p-6 *:before:absolute *:before:bg-gray-800 *:before:[block-size:100vh] *:before:[inline-size:1px] *:before:[inset-block-start:0] *:before:[inset-inline-start:-1px] *:after:absolute *:after:bg-gray-800 *:after:[block-size:1px] *:after:[inline-size:100vw] *:after:[inset-block-start:-1px] *:after:[inset-inline-start:0] md:*:p-10">
+            {
+              data.map((item, key) => {
+                return (
+                  <article key={key}>
+                    <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </h3>
+                    <p className="text-[15px] text-gray-400">
+                      {item.description}
+                    </p>
+                  </article>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
