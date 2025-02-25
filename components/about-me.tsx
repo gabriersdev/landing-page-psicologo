a7af457a6ecf123746cfd019ccada5225f09ebb7
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-02-25 14:23:16 -0300

    feat: componente sobre mim

---
+++ components/about-me.tsx
@@ -0,0 +1,19 @@
+import Badge from "@/components/badge";
+
+export default function AboutMe() {
+  return (
+    <section className={"mx-auto max-w-6xl px-4 sm:px-6"}>
+      <div className="pb-12 pt-32 md:pb-20 md:pt-40">
+        <div className="pb-12 text-center md:pb-16">
+          <Badge text={"Sobre mim"}/>
+          <h2 className={"mt-4 text-3xl font-bold text-emerald-600 md:text-4xl"}>O meu trabalho</h2>
+          <div className={"mt-8 sm:px-4 md:px-8"}>
+            <article>
+              <p className={"text-balance text-slate-700 text-[16px] md:text-xl"}>Joelson é um psicólogo clínico dedicado ao bem-estar emocional e ao desenvolvimento pessoal de seus pacientes. Formado em Psicologia, possui vasta experiência no atendimento de adultos e adolescentes, utilizando abordagens terapêuticas eficazes para promover autoconhecimento, equilíbrio emocional e qualidade de vida. Ao longo de sua carreira, Joelson se especializou no tratamento de ansiedade, depressão e outros desafios emocionais, sempre com um olhar acolhedor e humanizado. Seu compromisso é oferecer um espaço seguro e confidencial para que cada pessoa possa se expressar livremente e encontrar caminhos para uma vida mais plena.</p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
